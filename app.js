ac209f3267b17d512166c6139b0c068592af02ab
MustafaKashif <kashifmustafa822@gmail.com>
2023-09-09 10:13:44 +0500

    Mustafa Kashif

---
+++ app.js
@@ -0,0 +1,117 @@
+//! Chapter No. 05
+
+//! Question No. 01
+//?  var number1 = +prompt("Enter The First Number");
+//?  var number2 = +prompt("Enter the second number");
+//?  var sum = number1 + number2;
+
+//! Question No. 02
+//?  var sub = number1 - number2;
+//?  var multi = number1 * number2;
+//?  var div = number1 / number2;
+//? document.write("Sum of "+" "+number1+" "+ "and" + " " + number2+" "+ "is"+ " "+ sum);
+//? document.write("<br>Subtraction of "+" "+number1+" "+ "and" + " " + number2+" "+ "is"+ " "+ sub);
+//? document.write("<br>Multiplication of "+" "+number1+" "+ "and" + " " + number2+" "+ "is"+ " "+ multi);
+//? document.write("<br>Division of "+" "+number1+" "+ "and" + " " + number2+" "+ "is"+ " "+ div);
+
+//! Question No. 03
+//? var value;
+//? value = 5;
+//? var increment = ++value;
+//? var addition = value + 7;
+//? var decrement = addition--;
+//? var remainder = value % 3;
+//? document.write("<br>Value after varibale declaration is undefined");
+//? document.write("<br>Initial value of the variable is : " +value);
+//? document.write("<br>Value after increment is : " +increment);
+//? document.write("<br>Value after addition is : " +addition);
+//? document.write("<br>Value after decrement is : " +decrement);
+//? document.write("<br>The Remainder is  : " +remainder);
+
+//! Question No. 04
+//? var movie = 600;
+//? var total = movie * 5;
+//? document.write("<br>Total cost to buy 5 Tickets to a movie is "+total+" "+"PKR");   
+
+//! Question No. 05
+//? document.write("<br>Table of 10");
+//? document.write("<br>10 x 1 = "+(10*1));
+//? document.write("<br>10 x 2 = "+(10*2));
+//? document.write("<br>10 x 3 = "+(10*3));
+//? document.write("<br>10 x 4 = "+(10*4));
+//? document.write("<br>10 x 5 = "+(10*5));
+//? document.write("<br>10 x 6 = "+(10*6));
+//? document.write("<br>10 x 7 = "+(10*7));
+//? document.write("<br>10 x 8 = "+(10*8));
+//? document.write("<br>10 x 9 = "+(10*9));
+//? document.write("<br>10 x 10 = "+(10*10));
+
+//! Question No. 06
+//? var celsius = +prompt("Enter the temperature in Celsius : ");
+//? var Farhenheit = +prompt("Enter the temperature in fahrenheit : ");
+//? var ct = (Farhenheit - 32) * 5 / 9;
+//? var ft = (celsius * 9 / 5) + 32;
+//? document.write("<br>"+celsius + " " + "degree Celsius is "+" "+ft+" "+"degree Farhenheit");
+//? document.write("<br>"+Farhenheit + " " + "degree Farhenheit is "+" "+ct+" "+"degree Celsius");
+
+//! Question No. 07
+//? var item1 = 650;
+//? var item2 = 100;
+//? var quantity1 = 3;
+//? var quantity2 = 7;
+//? var ship = 100;
+//? var i1 = item1 * quantity1;
+//? var i2 = item2 * quantity2;
+//? var total = i1 + i2 + ship;
+//? document.write("<br>Total cost of your order is : "+total);
+
+//! Question No. 08
+//? document.write("<br>Marks Sheet");
+//? var totalmarks = 980;
+//? var obtainedmarks = 804;
+//? var percentage = (obtainedmarks/totalmarks) * 100;
+//? document.write("<br>Total Marks = " +totalmarks);
+//? document.write("<br>Obtained Marks = " +obtainedmarks);
+//? document.write("<br>Percentage = " +percentage);
+
+//! Question No. 09
+//? var dollars = 10;
+//? var riyaal = 25;
+//? var total = (10 * 308.80) + (25 * 80);
+//? document.write("<br>Currency Calculator");
+//? document.write("<br>Total currency in PKR is : "+total);
+
+//! Question No. 10
+//? var number = 2;
+//? var calculation = (number + 2 * 10 / 2);
+//? document.write("Total calculation is : "+calculation);
+
+//! Question No. 11
+//? var currentyear = 2022;
+//? var birthyear = +prompt("Enter your birth year : ");
+//? var age = currentyear - birthyear;
+//? document.write("<br>Current year is : "+currentyear);
+//? document.write("<br>Your birth year is : "+birthyear);
+//? document.write("<br>Your Age is : "+age);
+
+//! Question No. 12
+//? var radius = 20;
+//? var circumference = 2 * 3.142 * radius;
+//? var area = 3.142 * radius * radius;
+//? document.write("<br>The Geometrizer");
+//? document.write("<br>The Radius of a circle is : "+radius);
+//? document.write("<br>The Circumference of a circle is : "+circumference);
+//? document.write("<br>The Area of a circle is : "+area);
+
+//! Question No. 13
+//? var fs = "Biryani";
+//? var age = 19;
+//? var ageest = 55;
+//? var estimate = 2;
+//? document.write("<br>My favourite snack is : "+fs);
+//? document.write("<br>My Current age is : "+age);
+//? document.write("<br>Estimated age is : "+ageest);
+//? document.write("<br>The amount of snack i eat a day is : "+estimate);
+//?  document.write("<br>I will need 100"+" "+fs+" "+"to last you until the ripe old age of"+" "+ageest);
+
+//! Chapter No.05 Completed
